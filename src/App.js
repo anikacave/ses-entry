10cf5c8287c3e3666e60fc480549068911d0ddca
Anika Cave <asc287@cornell.edu>
2021-06-04 23:06:36 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+import * as $ from "jquery";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      // TODO: this is hardcoded u idiot
+      token: "3b3549ce",
+      s_info: {}
+      // search_bar: "",
+    }
+
+  }
+
+  movieReq(title) {
+    console.log("cinderella");
+    $.ajax({
+      url: "http://www.omdbapi.com/?t=cinderella&apikey=3b3549ce",
+      type: "GET",
+      success: data => {
+        console.log(data);
+        this.setState({
+          s_info: data
+        });
+      }
+    });
+
+  }
+
+  render() {
+    return (
+      <button type="submit" onClick={() => { this.movieReq("") }}>Send to Playlist app.js</button>
+
+    )
+
+  }
+
 }
 
 export default App;
