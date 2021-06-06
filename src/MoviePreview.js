4e8a3184fbf6b7b1653adf3bfd68f30ee41282ac
Anika Cave <asc287@cornell.edu>
2021-06-06 16:21:28 -0500

    better layout

--- src/MoviePreview.js
+++ src/MoviePreview.js
@@ -29,34 +29,34 @@ class MoviePreview extends React.Component {
     return (
       <React.Fragment>
         {this.state.data && (
-          <table>
-            <tbody>
-              {this.state.data.Search.map(id =>
-                <tr className="box" key={id.imdbID} >
-                  <td className="track_name"  >
-                    <button id={id.imdbID} onClick={() => this.setSelected(id.imdbID)}>
-                      <div>
-                        <h1>
-                          {id.Title} -- {id.Year}
-                        </h1>
-                      </div>
-                      {id.imdbID}
-                      <div>
-                        <img
-                          src={id.Poster}
-                          alt={id.imbdID}
-                        />
-                      </div>
-                    </button>
-                    {/* {show_image(id.Poster, 100, 300, "img")} */}
+          // <table>
+          //   <tbody>
+          <div>
+            {this.state.data.Search.map(id =>
+              // <tr className="box" key={id.imdbID} >
+              //   <td className="track_name"  >
+              <button id={id.imdbID} key={id.imdbID} onClick={() => this.setSelected(id.imdbID)}>
+                <div>
+                  <h1>
+                    {id.Title} -- {id.Year}
+                  </h1>
+                </div>
+                {id.imdbID}
+                <div>
+                  <img
+                    src={id.Poster}
+                    alt={id.imbdID}
+                  />
+                </div>
+              </button>
 
-                    {/* {id.Poster} */}
-
-                  </td>
-                </tr>)
-              }
-            </tbody>
-          </table>
+              //   </td>
+              // </tr>)
+            )
+            }
+          </div>
+          //   </tbody>
+          // </table>
         )
         }
       </React.Fragment>
